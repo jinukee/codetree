4536237b55b8ac6e61527970ac3f23d6f63a63b4
jinukee <87477498+jinukee@users.noreply.github.com>
2026-01-29 15:15:11 +0900

    Title: 반복 출력하기 2, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260129/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    private static final StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        recursive(n);
+        System.out.println(sb);
+    }
+
+    private static void recursive(int n) {
+        if(n == 0) {
+            return;
+        }
+
+        recursive(n - 1);
+        sb.append("HelloWorld").append("\n");
+    }
+}
