50e04c9a6c44726d3f24bb5b3220e261ace66776
jinukee <87477498+jinukee@users.noreply.github.com>
2026-02-12 16:22:43 +0900

    Title: 그래프 탐색, Time: 177ms, Memory: 14MB, Status: Passed - Codetree

---
+++ 260212/그래프 탐색/graph-traversal.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final List<List<Integer>> adj = new ArrayList<>();
+    private static int result = 0;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        dfs(1);
+        System.out.println(result);
+    }
+
+    private static void dfs(int vertex) {
+        visited[vertex] = true;
+        for (int neighbor : adj.get(vertex)) {
+            if (!visited[neighbor]) {
+                result++;
+                dfs(neighbor);
+            }
+        }
+    }
+}
