795e5e6e70120f3b86220aaa075c7fda8fbeec2b
jinukee <87477498+jinukee@users.noreply.github.com>
2026-02-13 14:21:14 +0900

    Title: 계단 오르기, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

--- 260213/계단 오르기/climbing-stairs.java
+++ 260213/계단 오르기/climbing-stairs.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         System.out.println(solution(N));
     }
 
@@ -18,9 +17,9 @@ public class Main {
         }
 
         int[] dp = new int[n + 1];
-        dp[1] = dp[2] = dp[3] = 1;
+        dp[2] = dp[3] = 1;
         for (int i = 4; i <= n; i++) {
-            dp[i] = Math.min(dp[i - 2] + 1, dp[i - 3] + 1) % MOD;
+            dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
         }
 
         return dp[n];
